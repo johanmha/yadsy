78e8d6ad0c90db0142dbb19ba05fe9f10b6e21c0
Johan Haugland <johanhaugl@gmail.com>
2025-02-26 07:58:59 +0100

    Init: add config for build and testing

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+    }),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+};
