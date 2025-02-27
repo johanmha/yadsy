f9385ab529a1599bc0ad17248b1d4e5688dc430b
Johan Haugland <johanhaugl@gmail.com>
2025-02-27 15:23:09 +0100

    Init: setup css inclusion in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
 import { readFileSync } from 'node:fs';
+import copy from 'rollup-plugin-copy';
 
 const pkg = JSON.parse(
   readFileSync(new URL('./package.json', import.meta.url), 'utf8')
@@ -27,6 +29,13 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
+    postcss({
+      extensions: ['.css'],
+      minimize: true,
+      inject: {
+        insertAt: 'top',
+      },
+    }),
     typescript({
       tsconfig: './tsconfig.json',
       outputToFilesystem: true,
